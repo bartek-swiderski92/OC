138c0556a5e5006b8f204e8530f4dcbc6385f610
Phos23 <phos23@gmail.com>
2020-11-07 13:52:08 +0000

    test v2

--- testing/jasmine-standalone-3.6.0/spec/BowlingGameSpec.js
+++ testing/jasmine-standalone-3.6.0/spec/BowlingGameSpec.js
@@ -1,26 +1,27 @@
-describe("Bowling game", function () {
-
+describe("BowlingGame", function () {
     var game;
+
     beforeEach(function () {
         game = new BowlingGame();
     });
 
     function rollMany(n, pins) {
         for (var i = 0; i < n; i++) {
-            game.roll(pins);
+            game.roll(pins)
         }
     }
 
-    it("should properly calculate a gutter game", function () {
-        game.roll(20, 0);
+    it("should handle a gutter game", function () {
+        rollMany(20, 0);
         expect(game.score()).toEqual(0);
     });
 
     it("should properly calculate a strike", function () {
-        //test
+        // test
     });
 
     it("should properly calculate a spare", function () {
         // test
-    })
+    });
+
 });
--- testing/jasmine-standalone-3.6.0/src/BowlingGame.js
+++ testing/jasmine-standalone-3.6.0/src/BowlingGame.js
@@ -1,13 +1,13 @@
-var BowlingGame = function () {
+var BowlingGame = function() {
     this.rolls = [];
     this.currentRoll = 0;
 };
 
-BowlingGame.prototype.roll = function (pins) {
+BowlingGame.prototype.roll = function(pins) {
     this.rolls[this.currentRoll++] = pins;
-}
+};
 
-BowlingGame.prototype.score = function () {
+BowlingGame.prototype.score = function() {
     var score = 0;
     var frameIndex = 0;
     var self = this;
